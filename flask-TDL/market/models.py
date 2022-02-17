2ceb4dc256be79de3268499b2a98852b94b8dc27
Ubuntu <jonasnoori@developmentserverjonas.fezafmtfxsnu1irtqzne1th2ra.zx.internal.cloudapp.net>
2022-02-17 19:19:52 +0000

    resturctured files

--- flask-TDL/market/models.py
+++ flask-TDL/market/models.py
@@ -1,11 +1,4 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-db = SQLAlchemy(app)
+from market import db
 
 class user(db.Model):
     id = db.Column(db.integer(), primary_key=True)
@@ -22,19 +15,5 @@ class Item(db.Model):
     barcode = db.Column(db.String(length=12), nullable=False,unique=True)
     description = db.Column(db.String(length=1000), nullable=False, unique=True)
     owner = db.Column(db.Integer(), db.Foreignkey('user.id'))
-
     def __repr__(self):
         return f'Item {self.name}'
-
-@app.route('/')
-@app.route('/home')
-def home_page():
-    return render_template('home.html')
-
-@app.route('/market')
-def market_page():
-    items = Item.query.all()
-    return render_template('market.html', items=items)
-
-if __name__=='__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
