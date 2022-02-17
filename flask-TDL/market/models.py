110d6abebec8fac072ea365ad15c83abfaa886b8
Ubuntu <jonasnoori@developmentserverjonas.fezafmtfxsnu1irtqzne1th2ra.zx.internal.cloudapp.net>
2022-02-17 20:11:54 +0000

    added port 500 to run file, made changes to databse

--- flask-TDL/market/models.py
+++ flask-TDL/market/models.py
@@ -1,7 +1,7 @@
 from market import db
 
-class user(db.Model):
-    id = db.Column(db.integer(), primary_key=True)
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
@@ -12,8 +12,10 @@ class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
-    barcode = db.Column(db.String(length=12), nullable=False,unique=True)
-    description = db.Column(db.String(length=1000), nullable=False, unique=True)
-    owner = db.Column(db.Integer(), db.Foreignkey('user.id'))
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+    
     def __repr__(self):
         return f'Item {self.name}'
