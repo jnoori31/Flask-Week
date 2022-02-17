483417dda1e232d7a77b6f3d268d3529f962a24c
Jonas Noori jonasgeneralassembly@gmail.com <jonasgeneralassembly@gmail.com>
2022-02-17 18:52:32 +0000

    user table and added new vm to config file

--- flask-TDL/market.py
+++ flask-TDL/market.py
@@ -7,12 +7,21 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 
+class user(db.Model):
+    id = db.Column(db.integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False,unique=True)
     description = db.Column(db.String(length=1000), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.Foreignkey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
--- flask-TDL/templates/notes.py
+++ flask-TDL/templates/notes.py
@@ -1,1 +1,2 @@
 # test url redirect on each page/ NAV bar market/home/
+# test each button works when redirecting
